bf9ca3ba28dd3fa990576d931450ddd56cee3bc5
Victor Poletti <victor.poletti@gastecnologia.com.br>
2019-06-11 12:51:59 -0300

    Adicionando conexao com mongodb

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true
+});
 
 app.get('/', (req, res) => {
   return res.send('olar');
