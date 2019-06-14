b747410a9b3f7309589c446507783ebcf1ec4600
Victor Poletti <victor.poletti@gastecnologia.com.br>
2019-06-14 09:47:46 -0300

    Adicionando eslint

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
 mongoose.connect(process.env.MONGODB_URL, {
-  useNewUrlParser: true
+  useNewUrlParser: true,
 });
 
 app.use((req, res, next) => {
@@ -21,10 +21,7 @@ app.use((req, res, next) => {
 
 app.use(cors());
 
-app.use(
-  '/files',
-  express.static(path.resolve(__dirname, '..', 'uploads', 'resized'))
-);
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(require('./routes'));
 
